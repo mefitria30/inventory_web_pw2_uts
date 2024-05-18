18d594be28859f57f1810c46dbc6473c32e0d785
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2024-05-18 20:01:34 +0700

    update total barang

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -14,7 +14,18 @@
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             Goods Total in Stocks</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+
+                                            <?php
+                                            $sql = "SELECT sum(jumlah_barang) as TotalBarang FROM master_data";
+                                            $result = $conn->query($sql);
+
+                                            while ($row = $result->fetch_assoc()) {
+                                                echo $row["TotalBarang"];
+                                            }
+                                        ?>
+
+                                        </div>
                                     </div>
                                     <div class="col-auto">
                                         <i class="fas fa-database fa-2x text-gray-300"></i>
